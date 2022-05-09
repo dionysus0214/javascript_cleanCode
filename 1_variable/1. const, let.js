a30fa0c52a9b34e4b4f1edc2e41b7f8495d00496
dionysus0214 <hiba0214@naver.com>
2022-05-09 22:26:03 +0900

    임시 변수를 제거해 함수 추상화

--- 1_variable/1. const, let.js
+++ 1_variable/1. const, let.js
@@ -38,7 +38,9 @@ city = 'busan';
 // 전역 공간 사용을 줄이기 위해선 IIFE, module, closure, const, let 등을 사용하는 것이 좋음
 
 // 임시 변수: scope 안에서 전역변수처럼 활용되는 것
-// 임시 변수가 함수 안에 생기면 CRUD 하고 싶은 유혹이 발생
+// 임시 변수를 사용할 경우 명령형으로 가득한 로직이 나오고 어디서 어떻게 잘못되었는지 디버깅이 어렵다는 문제 발생
+// 임시 변수가 함수 안에 생기면 CRUD 하고 싶은(추가적인 코드를 작성하고 싶은) 유혹이 발생
+// 임시 변수를 제거하는 방법: 함수 나누기, 바로 return, 고차함수(map, reduce, filter) 사용, 선언형 코드로 변경
 function getElements() {
   const result = {    
     title: document.querySelector('.title'),
@@ -47,4 +49,27 @@ function getElements() {
   };
 
   return result;
+}
+
+function getDateTime(targetDate) {
+  const month = targetDate.getMonth();
+  const day = targetDate.getDay();
+  const hour = targetDate.getHour();
+
+  return {
+    month: month >= 10 ? month : '0' + month,
+    day: day >= 10 ? day : '0' + day,
+    hour: hour >= 10 ? hour : '0' + hour,
+  }
+}
+
+// 임시 변수를 줄이면 함수 추상화 가능
+function getDateTime() {
+  const currentDateTime = getDateTime(new Date());
+
+  return {
+    month: currentDateTime.month + '월 전',
+    day: currentDateTime.day + '일 전',
+    hour: currentDateTime.hour + '시 전',
+  }
 }
