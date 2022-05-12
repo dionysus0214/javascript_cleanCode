517d5de5268f073855873cccb976c1d6020a6f00
dionysus0214 <hiba0214@naver.com>
2022-05-12 23:09:23 +0900

    undefiined, null

---
+++ 2_type/2_undefined, null.js
@@ -0,0 +1,10 @@
+// !null: true
+// !!null: false
+// null은 수학 연산 시 0으로 취급
+
+let vard;
+typeof vard; // 'undefined'
+let xard = null;
+typeof xard; // 'object'
+
+// undefined, null은 값이 없거나(없음을 명시적으로 표현한 것이 null) 정의가 되지 않은 것
