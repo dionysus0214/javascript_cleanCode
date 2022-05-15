f7797f2d41d09c879c289e31e03f2284c578bf46
dionysus0214 <hiba0214@naver.com>
2022-05-15 19:11:29 +0900

    isNaN

--- 2_type/3_typeCasting.js
+++ 2_type/3_typeCasting.js
@@ -1,3 +1,4 @@
+// 사용자가 '명시적인 형 변환(타입)'을 해줘야 문제가 생기지 않음
 // === strict equality
 // 동등 연산자를 사용하면 형변환(type casting) 발생
 '1' == 1 // true
@@ -9,4 +10,9 @@
 // parseInt 사용 시 뒤에 10을 넣어줘야 10진수라고 인식 가능
 parseInt('9.9999', 10) // 9
 
-// 사용자가 '명시적인 형 변환(타입)'을 해줘야 문제가 생기지 않음
+Number.isInteger // 자바스크립트에서 숫자를 검사할 때 정수인지 여부 판단
+// isNaN은 is not a number(숫자가 아니다)
+isNaN(123) // false, 숫자가 숫자가 아닌 게 아니다(= 숫자가 맞다)
+isNaN(123 + 'hi') // true
+Number.isNaN(123 + 'hi') // false
+// isNaN은 느슨한 검사, Number.isNaN은 엄격한 검사
