8f06cda2464e1a820a86c97c58aab3bbeac1daf7
dionysus0214 <hiba0214@naver.com>
2022-05-17 23:03:17 +0900

    begin/end 및 first/last를 활용한 경계 다루기

---
+++ 3_boundary/2_beginEnd.js
@@ -0,0 +1,12 @@
+// begin, end: 경계를 포함하지만 제외하는 경우
+function reservationDate(beginDate, endDate) {
+  // ...
+}
+reservationDate('YYYY-MM-DD', 'YYYY-MM-DD');
+
+// first, last: 포함된 양 끝을 의미(부터 ~까지)
+const students = ['poco', 'john', 'tom'];
+function getStudents(fitst, last) {
+  //
+}
+getStudents('poco', 'tom');
