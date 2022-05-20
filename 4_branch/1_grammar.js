344f652631b088dae39176de3178a6b0d28e6271
dionysus0214 <hiba0214@naver.com>
2022-05-20 19:42:46 +0900

    고차함수를 활용한 리팩토링

---
+++ 4_branch/1_grammar.js
@@ -0,0 +1,59 @@
+// 문법이 중요한 이유: 프로그래밍 '언어'이기 때문(컴퓨터를 이해시켜야 함)
+ReactDOM.render(
+  <div id="msg">Hello World!</div>,
+  mountNode
+);
+// babel을 만나면 아래처럼 객체로 transformed
+// 객체로 바뀌는 것 내부에는 값과 식만 넣을 수 있음(if, for, switch 등 넣을 수 없음)
+ReactDOM.render(React.createElement('div', {
+  id: 'msg',
+}, 'Hello World!'), mountNode);
+
+// before
+function ReactComponent() {
+  return (
+    <tbody>
+      {(() => {
+        const row = [];
+        for (let i = 0; i < objectRows.length; i++) {
+          rows.push(<ObjectRow key={i} data={objectRows[i]} />);
+        }
+        return rows;
+      })()}
+    </tbody>
+  );
+}
+// after: map 활용
+function ReactComponent() {
+  return (
+    <tbody>
+      {objectRows.map((obj, i) => (
+        <ObjectRow key={i} data={obj} />
+      ))}
+    </tbody>
+  );
+}
+
+// before
+function ReactComponent() {
+  return (
+    <div>
+      {(() => {
+        if (conditionOne) return <span>One</span>;
+        if (conditionTwo) return <span>Two</span>;
+        else conditionOne;
+        return <span>Three</span>;
+      })()}
+    </div>
+  );
+}
+// after
+function ReactComponent() {
+  return (
+    <div>
+      {conditionOne && <span>One</span>}
+      {conditionTwo && <span>Two</span>}
+      {!conditionTwo && <span>Three</span>}
+    </div>
+  );
+}
