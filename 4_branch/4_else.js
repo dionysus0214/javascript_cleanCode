d7127455ad08056945ed7eb8dd59992dc2ed3c9a
dionysus0214 <hiba0214@naver.com>
2022-05-24 22:12:40 +0900

    early return 적용

--- 4_branch/4_else.js
+++ 4_branch/4_else.js
@@ -28,3 +28,66 @@ function getActiveUserName() {
   }
   return '이름 없음';
 }
+
+// before
+function loginService(isLogin, user) {
+  if (!isLogin) {
+    if (checkToken()) {
+      if (!user.nickName) {
+        return registerUser(user);
+      } else {
+        refreshToken();
+        return '로그인 성공';
+      }
+    } else {
+      throw new Error('No Token');
+    }
+  }
+}
+// after: early return으로 함수를 미리 종료하고 사고하기 편한 방향으로 바꿈
+function login() {
+  refreshToken();
+  return '로그인 성공';
+}
+function loginService(isLogin, user) {
+  if (isLogin) {
+    return;
+  }
+  if (!checkToken()) {
+    throw new Error('No Token');
+  }
+  if (!user.nickName) {
+    return registerUser(user);
+  }
+  login();
+}
+
+// before
+function today(condition, weather, isJob) {
+  if (condition === 'GOOD') {
+    study();
+    exercise();
+    if (weather === 'GOOD') {
+      laundry();
+      sweep();
+    }
+    if (isJob === 'GOOD') {
+      ealrySleep();
+    }
+  }
+}
+// after
+function today(condition, weather, isJob) {
+  if (condition !== 'GOOD') {
+    return;
+  }
+  study();
+  exercise();
+  if (weather === 'GOOD') {
+    laundry();
+    sweep();
+  }
+  if (isJob === 'GOOD') {
+    ealrySleep();
+  }
+}
