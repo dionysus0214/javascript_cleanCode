ad6d18fc664784944d4fae5239196d607eecaca1
dionysus0214 <hiba0214@naver.com>
2022-05-29 22:52:45 +0900

    Array.length 활용 시 주의사항

--- 5_array/1_object.js
+++ 5_array/1_object.js
@@ -11,3 +11,19 @@ obj // { 0: 1, 1: 2, 2: 3 }
 
 const arr2 = '[1, 2, 3]';
 Array.isArray(arr2); // false
+
+// Array의 length는 배열의 길이라기 보단 배열의 마지막 인덱스에 가까움
+// 배열의 길이를 보장하지 못함
+const arr3 = [1, 2, 3];
+arr3[3] = 4;
+arr3.length // 4;
+arr[9] = 10;
+arr3 // [1, 2, 3, 4, , , , , , 10]
+arr3.length // 10
+
+function clearArray(array) {
+  array.length = 0;
+  return array;
+}
+const arr4 = [1, 2, 3];
+clearArray(arr4); // []
