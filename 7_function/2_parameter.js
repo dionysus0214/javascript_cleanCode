712775c0f8023814f73c1cdbf91a5ad111fd689a
dionysus0214 <hiba0214@naver.com>
2022-06-15 22:44:40 +0900

    함수 인자 관리

--- 7_function/2_parameter.js
+++ 7_function/2_parameter.js
@@ -4,3 +4,27 @@ function axios(url) {
 }
 // argument: actual parameter(실제로 사용되는 인자)
 axios('http://naver.com');
+
+// 복잡한 인자 관리하기
+// before
+function createCar(name, brand, color, type) {
+  return {
+    name,
+    brand,
+    color,
+    type
+  };
+}
+// after: 객체 구조 분해 할당을 통해 함수의 parameter를 받을 수 있음
+function createCar({ name, brand, color, type }) {
+  var name = options.name;
+  var brand = options.brand;
+  var color = options.color;
+  var type = options.type;
+  return {
+    name,
+    brand,
+    color,
+    type
+  };
+}
