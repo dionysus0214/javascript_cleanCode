63ac2f61deec852b579691a695f425d529358842
dionysus0214 <hiba0214@naver.com>
2022-06-17 23:49:08 +0900

    rest parameter 사용

--- 7_function/2_parameter.js
+++ 7_function/2_parameter.js
@@ -28,3 +28,19 @@ function createCar({ name, brand, color, type }) {
     type
   };
 }
+
+// rest parameter
+// before
+function sumTotal() {
+  return Array.from(arguments).reduce(
+    (acc, curr) => acc + curr,
+  );
+}
+sumTotal(1, 2, 3, 4, 5);
+// after: rest parameter를 통해 배열로 사용 가능(Array.from 사용 안 해도 됨)하며 다른 변수와 조합 가능
+function sumTotal(initValue, ...args) {
+  return args.reduce(
+    (acc, curr) => acc + curr, initValue
+  );
+}
+sumTotal(100, 1, 2, 3, 4, 5);
